e1b65f27556509b9338dd5049837455c6fd96afd
dreusus <pitak@dreusus.ru>
2025-04-15 17:23:18 +0300

    Исправления после ревью

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -11,7 +11,7 @@ PASSWORD_EMPTY: Final[str] = '  '
 
 @pytest.mark.parametrize('email, password',
                          [(EMAIL, PASSWORD), (EMAIL, PASSWORD_EMPTY), (EMAIL_EMPTY, PASSWORD)])
-def test_wrong_email_or_password_authorization(login_page:LoginPage, email: str, password: str) -> None:
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str) -> None:
     login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
     login_page.fill_login_form(email, password)
     login_page.click_login_button()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,10 +1,17 @@
 from typing import Final
 
+import pytest
+
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
+
 EMAIL: Final[str] = 'user.name@gmail.com'
 PASSWORD: Final[str] = 'password'
 USERNAME: Final[str] = 'username'
 
-def test_successful_registration(registration_page,dashboard_page) -> None:
+@pytest.mark.regression
+@pytest.mark.registration
+def test_successful_registration(registration_page:RegistrationPage,dashboard_page:DashboardPage) -> None:
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
     registration_page.fill_registration_form(username=USERNAME, password=PASSWORD, email=EMAIL)
     registration_page.click_registration_button()
