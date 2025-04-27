64abe6795ed8c9c403c2ae411a50db895d88cb2d
dreusus <pitak@dreusus.ru>
2025-04-27 10:39:23 +0300

    Знакомство с PageComponent. Практика работы с Page Component

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,11 +1,16 @@
 from playwright.sync_api import Page, expect
 
+from components.navigation.navbar_component import NavbarComponent
+from components.navigation.sidebar_component import SidebarComponent
 from pages.base_page import BasePage
 
 
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.sidebar = SidebarComponent(page)
+        self.navbar = NavbarComponent(page)
 
         self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
@@ -35,9 +40,7 @@ class CoursesListPage(BasePage):
         expect(self.empty_view_title).to_have_text('There is no results')
 
         expect(self.empty_view_description).to_be_visible()
-        expect(self.empty_view_description).to_have_text(
-            'Results from the load test pipeline will be displayed here'
-        )
+        expect(self.empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
 
     def check_visible_create_course_button(self):
         expect(self.create_course_button).to_be_visible()
@@ -45,14 +48,7 @@ class CoursesListPage(BasePage):
     def click_create_course_button(self):
         self.create_course_button.click()
 
-    def check_visible_course_card(
-            self,
-            index: int,
-            title: str,
-            max_score: str,
-            min_score: str,
-            estimated_time: str
-    ):
+    def check_visible_course_card(self, index: int, title: str, max_score: str, min_score: str, estimated_time: str):
         expect(self.course_image.nth(index)).to_be_visible()
 
         expect(self.course_title.nth(index)).to_be_visible()
@@ -65,9 +61,7 @@ class CoursesListPage(BasePage):
         expect(self.course_min_score_text.nth(index)).to_have_text(f'Min score: {min_score}')
 
         expect(self.course_estimated_time_text.nth(index)).to_be_visible()
-        expect(self.course_estimated_time_text.nth(index)).to_have_text(
-            f'Estimated time: {estimated_time}'
-        )
+        expect(self.course_estimated_time_text.nth(index)).to_have_text(f'Estimated time: {estimated_time}')
 
     def click_edit_course(self, index: int):
         self.course_menu_button.nth(index).click()
