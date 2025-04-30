33e41c1eaad315245b4bf9d83ba879a40f21afd0
dreusus <pitak@dreusus.ru>
2025-04-30 19:42:13 +0300

    Реализация и использование компонента CoursesListToolbarViewComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -32,8 +32,7 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
                                                estimated_time="2 weeks")
     create_course_page.click_create_course_button()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.course_view.check_visible(index=0,
                                                 title="Playwright",
                                                 max_score="100",
@@ -49,6 +48,5 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.navbar.check_visible(username='username')
     courses_list_page.sidebar.check_visible()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
