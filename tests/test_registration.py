bcb0c0004360f0a9d93a990e7a341302cbb7621d
dreusus <pitak@dreusus.ru>
2025-05-01 20:28:34 +0300

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -9,11 +9,13 @@ EMAIL: Final[str] = 'user.name@gmail.com'
 PASSWORD: Final[str] = 'password'
 USERNAME: Final[str] = 'username'
 
+
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(registration_page:RegistrationPage,dashboard_page:DashboardPage) -> None:
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage) -> None:
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.fill_registration_form(username=USERNAME, password=PASSWORD, email=EMAIL)
+    registration_page.registration_form.fill(username=USERNAME, password=PASSWORD, email=EMAIL)
+    registration_page.registration_form.check_visible(username=USERNAME, password=PASSWORD, email=EMAIL)
     registration_page.click_registration_button()
 
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.toolbar_view_component.check_visible()
