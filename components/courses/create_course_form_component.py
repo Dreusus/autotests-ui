5bad91788513c94b352fb0bc903083039548bc47
Dreusus <pitakxdd@gmail.com>
2025-05-17 11:59:09 +0300

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -9,11 +9,11 @@ class CreateCourseFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.title_input = Input(page,'create-course-form-title-input','Title')
-        self.estimated_time_input = Input(page,'create-course-form-estimated-time-input','Estimated Time')
-        self.description_textarea = Textarea(page,'create-course-form-description-input','Description')
-        self.max_score_input = Input(page,'create-course-form-max-score-input','Max Score')
-        self.min_score_input = Input(page,'create-course-form-min-score-input','Min Score')
+        self.title_input = Input(page, 'create-course-form-title-input', 'Title')
+        self.estimated_time_input = Input(page, 'create-course-form-estimated-time-input', 'Estimated Time')
+        self.description_textarea = Textarea(page, 'create-course-form-description-input', 'Description')
+        self.max_score_input = Input(page, 'create-course-form-max-score-input', 'Max Score')
+        self.min_score_input = Input(page, 'create-course-form-min-score-input', 'Min Score')
 
     def check_visible(self, title: str, estimated_time: str, description: str, max_score: str, min_score: str):
         self.title_input.check_visible()
