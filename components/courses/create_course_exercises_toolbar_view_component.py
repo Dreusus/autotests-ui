cabe30700f845b669c4915c4cbcc9651eaec4da2
Dreusus <pitakxdd@gmail.com>
2025-05-17 18:08:24 +0300

    Добавляем allure.step в компоненты Page Component

--- components/courses/create_course_exercises_toolbar_view_component.py
+++ components/courses/create_course_exercises_toolbar_view_component.py
@@ -1,5 +1,4 @@
-
-
+import allure
 from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
@@ -11,13 +10,16 @@ class CreateCourseExercisesToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.title = Text(page,'create-course-exercises-box-toolbar-title-text','Title')
-        self.create_exercise_button = Button(page,'create-course-exercises-box-toolbar-create-exercise-button','Create Exercise')
+        self.title = Text(page, 'create-course-exercises-box-toolbar-title-text', 'Title')
+        self.create_exercise_button = Button(page, 'create-course-exercises-box-toolbar-create-exercise-button',
+                                             'Create Exercise')
+
 
     def check_visible(self):
         self.title.check_visible()
         self.title.check_have_text('Exercises')
         self.create_exercise_button.check_visible()
+
 
     def click_create_exercise_button(self):
         self.create_exercise_button.click()
